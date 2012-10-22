b089dd9770863d9fe975b59116566d99803c553f
Christopher Hlubek <hlubek@networkteam.com>
2012-10-22 14:13:12 +0200

    [TASK] Initial commit

---
+++ Classes/TYPO3/SingleSignOn/Client/Controller/StandardController.php
@@ -0,0 +1,31 @@
+<?php
+namespace TYPO3\SingleSignOn\Client\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.SingleSignOn.Client".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Standard controller for the TYPO3.SingleSignOn.Client package 
+ *
+ * @Flow\Scope("singleton")
+ */
+class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * Index action
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+		$this->view->assign('foos', array(
+			'bar', 'baz'
+		));
+	}
+
+}
+
+?>
